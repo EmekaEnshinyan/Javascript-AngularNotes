f98b4519c22ab4a40e8d3bfbc801dd0407c57f08
Emeka Anekwe <59901971+EmekaEnshinyan@users.noreply.github.com>
2021-06-18 17:57:05 -0700

    Update angular_quiz.js

--- angular_quiz.js
+++ angular_quiz.js
@@ -1,1 +1,30 @@
 
+//What will be the result of the below program?
+
+function f(input: boolean) {
+let a = 100;
+if (input) {
+let b = a + 1;
+}
+return b;
+}
+
+//In Angular, you can pass data from the parent component to the child component by using:
+
+ @Output()
+ @Input()
+ Input
+ Output
+ 
+ //In Angular, you can pass data from the child component to the parent component by using:
+
+ @Output()
+ @Input()
+ Input
+ Output
+ 
+ //Which directive connects the value of the controls to the data?
+
+ ng-app
+ ng-init
+ ng-model
